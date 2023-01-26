3730f7762f709b48324e536426933a5932218936
aysenuraltinsoy <104261402+aysenuraltinsoy@users.noreply.github.com>
2023-01-26 22:02:13 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+const app = express();
+
+let items = ["Buy Food", "Cook Food"];
+let workItems = [];
+
+app.set("view engine", "ejs"); //app oluşturduktan sonra ejsi adapte etmek önemli,
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  let day = date.getDay();
+  res.render("list", { listTitle: day, newListItems: items });
+});
+
+app.post("/", function (req, res) {
+  let item = req.body.newItem;
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+
+    res.redirect("/");
+  }
+});
+
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: workItems });
+});
+
+app.post("/work", function (req, res) {
+  var item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+});
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
